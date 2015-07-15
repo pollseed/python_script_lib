37d21ddc12dbf7837baff248bc7413dc0b186ed0
pollseed <cb.so.devope+0001@gmail.com>
2015-07-15 18:38:34 +0900

    Update work_time.py

--- lib/work_time.py
+++ lib/work_time.py
@@ -2,7 +2,6 @@ from datetime import datetime as dt
 from datetime import date as d
 
 class WorkTime(object):
-        LAST_HOUR = {}
         DAY_OF_THE_WEEK = {0:"monday",1:"tuesday",2:"wednesday",3:"thursday",4:"friday",5:"satarday",6:"sunday"}
         HOUR = 60
 
@@ -10,8 +9,7 @@ class WorkTime(object):
                 for k,v in self.DAY_OF_THE_WEEK.items():
                         if k == weekday:
                                 print('What time is working end time of {0}?'.format(v))
-                                self.LAST_HOUR[k] = int(input())
-                return
+                                return int(input())
 
         def get_time(self,hour):
                 rest = (lambda x: round(float(((hour - x.hour) * self.HOUR - x.minute) / self.HOUR), 3))(dt.now())
@@ -19,9 +17,7 @@ class WorkTime(object):
                 print('resttime :{0}h'.format(rest))
 
         def __init__(self):
-                weekday = d.today().weekday()
-                self.set_value(weekday)
-                self.get_time(self.LAST_HOUR[weekday])
+                self.get_time(self.set_value(d.today().weekday()))
 
 if __name__ == '__main__':
         WorkTime()
