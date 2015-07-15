e2faad5e88f949ed27f2dc0df480f36aadbe8675
pollseed <cb.so.devope+0001@gmail.com>
2015-07-15 21:51:37 +0900

    Update work_time.py

--- lib/work_time.py
+++ lib/work_time.py
@@ -8,26 +8,25 @@ What time is working end time of {__day of the week__}?
 Plerase your today's working-end hour >>>
 """
 class WorkTime(object):
-        DAY_OF_THE_WEEK = {0:"monday",1:"tuesday",2:"wednesday",3:"thursday",4:"friday",5:"satarday",6:"sunday"}
-        HOUR = 60
+    DAY_OF_THE_WEEK = {0:"monday",1:"tuesday",2:"wednesday",3:"thursday",4:"friday",5:"satarday",6:"sunday"}
+    HOUR = 60
 
-        def set_value(self,weekday):
-                for k,v in self.DAY_OF_THE_WEEK.items():
-                        if k == weekday:
-                                print('What time is working end time of {0}?'.format(v))
-                                ending_time = int(input())
-                                if ending_time > 0 and ending_time > dt.now().hour:
-                                        return ending_time
-                                raise Exception('0 > input value and now-time')
+    def set_value(self,weekday):
+        for k,v in self.DAY_OF_THE_WEEK.items():
+            if k == weekday:
+                print('What time is working end time of {0}?'.format(v))
+                ending_time = int(input())
+                if ending_time > 0 and ending_time > dt.now().hour:
+                    return ending_time
+                raise Exception('0 > input value and now-time')
 
-        def get_time(self,hour):
-                rest = (lambda x: round(float(((hour - x.hour) * self.HOUR - x.minute) / self.HOUR), 3))(dt.now())
-                print('lasttime :{0}h'.format(hour))
-                print('resttime :{0}h'.format(rest))
+    def get_time(self,hour):
+        rest = (lambda x: round(float(((hour - x.hour) * self.HOUR - x.minute) / self.HOUR), 3))(dt.now())
+        print('lasttime :{0}h'.format(hour))
+        print('resttime :{0}h'.format(rest))
 
-        def __init__(self):
-                self.get_time(self.set_value(d.today().weekday()))
+    def __init__(self):
+        self.get_time(self.set_value(d.today().weekday()))
 
 if __name__ == '__main__':
-        WorkTime()
-
+    WorkTime()
