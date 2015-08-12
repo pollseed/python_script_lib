9a011cc3cacd7e812c5291146c0950a6b08bcfed
pollseed <cb.so.devope+0001@gmail.com>
2015-08-12 18:52:22 +0900

    Update day.php

--- lib/php/day.php
+++ lib/php/day.php
@@ -3,5 +3,13 @@ const FORMAT = "Y-m-d";
 function getAddDays($days) {
     return date(FORMAT, strtotime("+".$days." days", strtotime(date(FORMAT))));
 }
-echo getAddDays(10);
+function getAddMonth($month) {
+    return date(FORMAT, strtotime("+".$month." month", strtotime(date(FORMAT))));
+}
+function getAddYear($year) {
+    return date(FORMAT, strtotime("+".$year." year", strtotime(date(FORMAT))));
+}
+echo getAddDays(10)."\n";
+echo getAddMonth(1)."\n";
+echo getAddYear(1);
 ?>
