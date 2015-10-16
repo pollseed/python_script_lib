863feb61c44e2b41d930bc5816d17df37a06c303
pollseed <cb.so.devope+0001@gmail.com>
2015-10-16 12:50:06 +0900

    Update and rename day.php to util.php

--- lib/php/util.php
+++ lib/php/util.php
@@ -12,4 +12,16 @@ function getAddYear($year) {
 echo getAddDays(10)."\n";
 echo getAddMonth(1)."\n";
 echo getAddYear(1);
+
+/**
+ * 指定した配列を指定した引数で文字結合します.
+ * @param $arr 配列
+ * @param $arg 引数
+ * @param $exclude default="strlent"
+ * @return 結合した文字列
+ **/
+function implodeFilter($arr, $arg, $exclude = "strlen") {
+    return implode($arg, array_values(array_filter($arr, $exclude)));       
+}
+echo implodeFilter(array("hoge","","fuga",null,"piyo",null),"-"));
 ?>
