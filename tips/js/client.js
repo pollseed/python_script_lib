608d1e87f4d0d4507ce94a9c98a536f4638dba70
pollseed <cb.so.devope+0001@gmail.com>
2015-10-19 17:01:06 +0900

    Update client.js

--- tips/js/client.js
+++ tips/js/client.js
@@ -1,67 +1,147 @@
 (function() {
     "use strict";
 
-    const IO = io(), SPACE = ' ';
+    const ERROR_CODE_BLANK = -1;
 
-    let Request = {
-        get: (url, query) => {
-            let defer = $.Deferred();
-            $.ajax( {
-                url: url + query,
-                type: 'GET',
-                success: defer.resolve,
-                error: defer.reject
-            });
-            return defer.promise();
-        }};
+    let StringUtils = {
+        isEmpty: str => {
+            let __str = __parseStr(str);
+            return (__str === null) || (__str.length <= 0);
+        },
+        isNotEmpty: str => {
+            return !isEmpty(str);
+        },
+        isBlank: str => {
+            const BLANK_CHR = [" ", "　"];
+            let __str = __parseStr(str), strLen = __str.length, i, ch;
+            if (__str === null || strLen === 0) return true;
+            for (i = 0; i < strLen; i++) {
+                ch = __str.charAt(i);
+                if (ERROR_CODE_BLANK == BLANK_CHR.indexOf(ch)) {
+                    return false;
+                }
+            }
+            return true
+        },
+        isNotBlank: str => {
+            return !isBlank(str);
+        },
+        trim: str => {
+            let __str = __parseStr(str);
+            return __str === null ? null : __str.trim();
+        },
+        trimToNull: str => {
+            let __str = this.trim(str);
+            return (__str === null) || (__str.length === 0) ? null : __str;
+        },
 
-    function ajaxRequest() {
-        let wordResultDom = new InputDom("word_result"),
-        result = wordResultDom.getIdDom(),
-        wordDom = new InputDom("word"),
-            word = wordDom.getInputValue(),
-            request, yahooUrl, githubUrl, stackoverflowUrl;
-        if (ERROR_CODE_BLANK === word) {
-            result.innerText = SPACE;
-            return;
-        } else if (wordDom.isNotEquals(word)) {
-            result.innerText = word;
+            /*
+             * create number of str element array.
+             * @param str element.
+             * @limit number of array.
+             */
+        createPlaceholder: (str, limit) => {
+            let placeholder = `${str},`.repeat(limit).split(',');
+            placeholder.pop();
+            return placeholder;
         }
-        IO.emit('server', {word: word});
-        request = new RequestUrls();
-        yahooUrl = request.getUrl('YAHOO', word);
-        githubUrl = request.getUrl('GITHUB', word);
-        stackoverflowUrl = request.getUrl('STACKOVERFLOW', word);
-        console.info(`request url : \r\n${yahooUrl}\r\n${githubUrl}\r\n${stackoverflowUrl}`);
+    };
 
-        Request.get(yahooUrl, word)
-            .done(data => { console.log(data) })
-            .fail(e => { console.log(e) });
-        Request.get(githubUrl, word)
-            .done(data => { console.log(data) })
-            .fail(e => { console.log(e) });
-        Request.get(stackoverflowUrl, word)
-            .done(data => { console.log(data) })
-            .fail(e => { console.log(e) });
+    function __parseStr(str) {
+        return `${str}`;
     }
 
-    /**
-     * ex.)
-     * itemテーブルにname,buy_dateカラムに値を突っ込んだレコードを100件追加する場合
-     */
-    function createSql() {
-        let sql = new Sql('item'), valuesArray = [], i;
-        for (i = 0; i < 100; i++) valuesArray.push([`"product${i}"`, 'now()']);
-        sql.createInsert(['name', 'buy_date'], valuesArray).forEach(v => console.log(v));
+    class Dom {
+        constructor(id) {
+            this._id = document.getElementById(id);
+        }
+        getIdDom() {
+            return this._id;
+        }
+        isNotEquals(text) {
+            return this.getIdDom().innerText !== text;
+        }
     }
 
-    function load() {
-        new InputDom("word").getIdDom().addEventListener('blur', ajaxRequest, false);
-        new InputDom("remain_time").getIdDom().addEventListener('click', () => {
-            let resultDom = new InputDom("remain_result").getIdDom(),
-            remain = new Timer("remain_time").getRemainTime();
-            resultDom.innerText = `${remain.hour} : ${remain.minute}`;
-        }, false);
+    class InputDom extends Dom {
+        getInputValue() {
+            let value = super.getIdDom().value;
+            if (StringUtils.isBlank(value)) {
+                console.info(`not blank, errorCode [${ERROR_CODE_BLANK}]`);
+                return ERROR_CODE_BLANK;
+            }
+            return value;
+        }
     }
-    document.addEventListener('DOMContentLoaded', load, false);
-}());
+
+    class RequestUrls {
+        constructor() {
+            this.__urls = new Map()
+                .set('YAHOO', 'http://search.yahoo.co.jp/search?p=')
+                .set('GITHUB', 'https://github.com/search?utf8=✓&q=')
+                .set('STACKOVERFLOW', 'http://stackoverflow.com/search?q=')
+                .set('NONE', '');
+        }
+        getUrl(serviceName, searchWord) {
+            let url = this.__urls.get(serviceName);
+            return url + searchWord;
+        }
+    }
+
+
+    class Timer {
+        constructor() {
+            this.__times = new Map()
+                .set(1, {hour: 17, minute: 45})
+                .set(2, {hour: 17, minute: 45})
+                .set(3, {hour: 17, minute: 45})
+                .set(4, {hour: 17, minute: 45})
+                .set(5, {hour: 17, minute: 45})
+                .set(6, {hour: 0, minute: 0})
+                .set(7, {hour: 0, minute: 0});
+        }
+        getRemainTime() {
+            let d = new Date(),
+            now = this.__times.get(d.getDay()),
+            hour = now.hour - d.getHours(),
+                minute = now.minute - d.getMinutes();
+            if (minute < 0) { hour--; minute+=60; }
+            return { hour: hour, minute: minute };
+        }
+    }
+
+    class Sql {
+        constructor(tblName) {
+            this.__tblName = tblName;
+        }
+        createInsert(columnNames, values_array) {
+            if (columnNames === null || columnNames === undefined
+                    || values_array === null || values_array === undefined) return false;
+            let sql_column = '', sql_value = '', sqls = [], i = 0, j = 0;
+            columnNames.forEach(v => {
+                if (i !== 0) sql_column += ',';
+                sql_column += v;
+                i++;
+            });
+            values_array.forEach(values => {
+                values.forEach(v => {
+                    if (j !== 0) sql_value += ',';
+                    sql_value += v;
+                    j++;
+                });
+                sqls.push(`insert into ${this.__tblName} (${sql_column}) values (${sql_value});`);
+                j = 0;
+                sql_value = '';
+            });
+            return sqls;
+        }
+    }
+
+    this.ERROR_CODE_BLANK = ERROR_CODE_BLANK;
+    this.StringUtils = StringUtils;
+    this.InputDom = InputDom;
+    this.RequestUrls = RequestUrls;
+    this.Timer = Timer;
+    this.Sql = Sql;
+
+}).call(this);
