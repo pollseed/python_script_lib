5d5f75668474b567bc0ec9f98c11e0b15768d7fa
pollseed <cb.so.devope+0001@gmail.com>
2015-10-20 11:43:14 +0900

    Update utils.js

--- candidate/js/lib/utils/utils.js
+++ candidate/js/lib/utils/utils.js
@@ -1,7 +1,7 @@
 (function() {
     "use strict";
 
-    const BLANK_CHR = [" ", "　"], ERROR_CODE_BLANK = -1;
+    const ERROR_CODE_BLANK = -1;
 
     let StringUtils = {
         isEmpty: str => {
@@ -12,16 +12,12 @@
             return !isEmpty(str);
         },
         isBlank: str => {
-            let __str = __parseStr(str),
-            strLen = __str.length,
-                i,
-                ch;
-            if (__str === null || strLen === 0) {
-                return true;
-            }
+            const BLANK_CHR = [" ", "　"];
+            let __str = __parseStr(str), strLen = __str.length, i, ch;
+            if (__str === null || strLen === 0) return true;
             for (i = 0; i < strLen; i++) {
                 ch = __str.charAt(i);
-                if (-1 == BLANK_CHR.indexOf(ch)) {
+                if (ERROR_CODE_BLANK == BLANK_CHR.indexOf(ch)) {
                     return false;
                 }
             }
@@ -106,8 +102,8 @@
         }
         getRemainTime() {
             let d = new Date(),
-                now = this.__times.get(d.getDay()),
-                hour = now.hour - d.getHours(),
+            now = this.__times.get(d.getDay()),
+            hour = now.hour - d.getHours(),
                 minute = now.minute - d.getMinutes();
             if (minute < 0) { hour--; minute+=60; }
             return { hour: hour, minute: minute };
@@ -140,7 +136,6 @@
             return sqls;
         }
     }
-
 
     this.ERROR_CODE_BLANK = ERROR_CODE_BLANK;
     this.StringUtils = StringUtils;
