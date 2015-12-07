07f77486d6e3face2356eae07d8e7ff5514171bc
pollseed <cb.so.devope+0001@gmail.com>
2015-12-07 17:40:22 +0900

    Create main.rb

---
+++ lib/ruby/main.rb
@@ -0,0 +1,19 @@
+require './lib/util'
+
+class MainExecute
+    ARR = [1,2,3]
+
+    attr_accessor :u
+
+    def initialize
+        @u = Util.instance
+    end
+
+    def main
+        p @u.time
+        @u.p ARR
+        p @u.sum ARR
+    end
+end
+
+MainExecute.new.main
