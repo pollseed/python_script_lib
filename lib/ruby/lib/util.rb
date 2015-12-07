928254a738900c65ce49252a978d19412edf1d78
pollseed <cb.so.devope+0001@gmail.com>
2015-12-07 17:39:58 +0900

    Create util.rb

---
+++ lib/ruby/lib/util.rb
@@ -0,0 +1,17 @@
+require 'singleton'
+
+class Util
+    include Singleton
+
+    def time
+        Time.new
+    end
+
+    def p vs
+        (-> vs{vs.each{|v|puts v}}).call vs
+    end
+
+    def sum vs
+        a=0;(-> vs{vs.each{|v|a+=v}}).call vs;a
+    end
+end
