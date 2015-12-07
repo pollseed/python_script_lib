c799033c79e6d1d830627154d0777629628b1160
pollseed <cb.so.devope+0001@gmail.com>
2015-12-07 17:51:40 +0900

    Update util.rb

--- lib/ruby/lib/util.rb
+++ lib/ruby/lib/util.rb
@@ -1,17 +1,19 @@
 require 'singleton'
 
-class Util
-    include Singleton
+module Util
+    class RubyPractice
+        include Singleton
 
-    def time
-        Time.new
-    end
+        def time
+            Time.new
+        end
 
-    def p vs
-        (-> vs{vs.each{|v|puts v}}).call vs
-    end
+        def p vs
+            (-> vs{vs.each{|v|puts v}}).call vs
+        end
 
-    def sum vs
-        a=0;(-> vs{vs.each{|v|a+=v}}).call vs;a
+        def sum vs
+            a=0;(-> vs{vs.each{|v|a+=v}}).call vs;a
+        end
     end
 end
