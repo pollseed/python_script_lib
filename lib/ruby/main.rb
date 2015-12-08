21b5442811ae1a166d8f937fb350a49b2e2d6753
pollseed <cb.so.devope+0001@gmail.com>
2015-12-08 13:40:52 +0900

    Update main.rb

--- lib/ruby/main.rb
+++ lib/ruby/main.rb
@@ -1,6 +1,7 @@
 require 'pp'
 require 'yaml'
 require './lib/util'
+require './lib/sort'
 
 class MainGenerator
     def initialize
@@ -11,11 +12,21 @@ class MainGenerator
 end
 
 class MainExecute < MainGenerator
-    def main
+    def util
         pp @u.time
         @u.p ARR
         pp @u.sum ARR
+        self
+    end
+
+    def bubble_sort
+        pp BubbleSort.new.sort [54,23,0,3,89,210,20,1,23,567,90]
+    end
+
+    def c
+        p caller
+        self
     end
 end
 
-MainExecute.new.main
+MainExecute.new.util.bubble_sort.c
