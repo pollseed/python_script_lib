ae18ab1f7dd49579be8b39132fdc0a35569476e5
pollseed <cb.so.devope+0001@gmail.com>
2015-12-09 12:19:43 +0900

    Update sort.rb

--- lib/ruby/lib/sort.rb
+++ lib/ruby/lib/sort.rb
@@ -6,8 +6,8 @@ class BubbleSort
         len,is_loop,ai=arr.length,true,0
         while is_loop
             ai = 0
-            (len-1).times {|i|
-                if arr[i] > arr[i+1] then arr[i+1],arr[i] = arr[i], arr[i+1]
+            (len-1).times {|_|
+                if arr[_] > arr[_+1] then arr[_+1],arr[_] = arr[_], arr[_+1]
                 else ai += 1
                 end
                 is_loop=false if ai==len-1
