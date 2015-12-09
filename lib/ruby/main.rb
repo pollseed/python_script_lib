3dc619efd2b3070ce184b0d4a37a2daee253844a
pollseed <cb.so.devope+0001@gmail.com>
2015-12-09 11:41:14 +0900

    Update main.rb

--- lib/ruby/main.rb
+++ lib/ruby/main.rb
@@ -20,22 +20,29 @@ class MainExecute < MainGenerator
     end
 
     def bubble_sort
-        r = ->(a,b,c){(rand(1..1000)*10*a*b-b) <<c}
-        test = []
-        (100).times {|v|
-            if v % 15 == 0 then test[v] = r.call 15,v,3
-            elsif v % 3 == 0 then test[v] = r.call 3,v,10
-            elsif v % 5 == 0 then test[v] = r.call 5,v,7
-            else test[v] = r.call 1,v,2
-            end
-        }
-        pp BubbleSort.new.sort test
+        pp BubbleSort.new.sort create_test_arr 100
         self
     end
 
     def c
         p caller
         self
+    end
+
+    private
+    def create_test_arr l
+        test = []
+        (l).times{|v|test[v]=fizzbuzz v}
+        test
+    end
+
+    def fizzbuzz v
+        r = ->a,b,c{(rand(1..1000)*10*a*b-b) <<c}
+        if v % 15 == 0 then r.call 15,v,3
+        elsif v % 3 == 0 then r.call 3,v,10
+        elsif v % 5 == 0 then r.call 5,v,7
+        else r.call 1,v,2
+        end
     end
 end
 
